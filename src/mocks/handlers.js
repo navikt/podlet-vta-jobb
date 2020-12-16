7cd5fbb7e1d235d41c6f2764fb9346e4a7df388b
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-12-16 15:13:49 +0100

    Mocker opp jobbsøkerbesvarelse

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -2,9 +2,11 @@ import get from "./msw-utils";
 import underOppfolging from "./under-oppfolging.json";
 import oppfolging from "./oppfolging.json";
 import auth from "./auth.json";
+import jobbsokerbesvarelse from "./jobbsokerbesvarelse.json";
 
 export const handlers = [
   get("https://api.nav.no/dittnav-api/oppfolging", underOppfolging),
   get("https://api.nav.no/vta-api/oppfolging", oppfolging),
+  get("https://api.nav.no/vta-api/jobbsokerbesvarelse", jobbsokerbesvarelse),
   get("https://api.nav.no/innloggingsstatus/auth", auth),
 ];
