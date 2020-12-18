678e4ba6dd865dd8eeeae4098a9426dd893e4b8f
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-12-18 08:18:19 +0100

    Fjerne registreringsinfo (var ikke behov for den)

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -3,7 +3,6 @@ import underOppfolging from "./under-oppfolging.json";
 import oppfolging from "./oppfolging.json";
 import auth from "./auth.json";
 import jobbsokerbesvarelse from "./jobbsokerbesvarelse.json";
-import registrering from "./registrering.json";
 
 export const handlers = [
   get("https://api.nav.no/dittnav-api/oppfolging", underOppfolging),
@@ -11,5 +10,4 @@ export const handlers = [
   get("https://api.nav.no/vta-api/jobbsokerbesvarelse", jobbsokerbesvarelse),
   get("https://api.nav.no/innloggingsstatus/auth", auth),
   get("https://api.nav.no/innloggingsstatus/auth", auth),
-  get("https://api.nav.no/vta-api/registrering", registrering),
 ];
