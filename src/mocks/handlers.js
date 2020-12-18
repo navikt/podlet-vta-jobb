59f5d5d04f11e88c044836e417045476d4cb601a
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-12-18 07:37:04 +0100

    legger til mock for registrering

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -3,10 +3,13 @@ import underOppfolging from "./under-oppfolging.json";
 import oppfolging from "./oppfolging.json";
 import auth from "./auth.json";
 import jobbsokerbesvarelse from "./jobbsokerbesvarelse.json";
+import registrering from "./registrering.json";
 
 export const handlers = [
   get("https://api.nav.no/dittnav-api/oppfolging", underOppfolging),
   get("https://api.nav.no/vta-api/oppfolging", oppfolging),
   get("https://api.nav.no/vta-api/jobbsokerbesvarelse", jobbsokerbesvarelse),
   get("https://api.nav.no/innloggingsstatus/auth", auth),
+  get("https://api.nav.no/innloggingsstatus/auth", auth),
+  get("https://api.nav.no/vta-api/registrering", registrering),
 ];
