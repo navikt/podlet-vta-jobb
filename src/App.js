0b547497c4844a6195f19fbb1b33ac75a5716efd
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-12-18 07:49:26 +0100

    Henter inn registreringsdata

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import useSWR from "swr";
 import "./App.css";
 import Jobb from "./components/jobb";
-import { authUrl, oppfolgingUrl, underOppfolgingUrl, besvarelseUrl } from "./config";
+import { authUrl, oppfolgingUrl, underOppfolgingUrl, besvarelseUrl, registreringUrl } from "./config";
 
 const fetcher = async (url) => {
   const response = await fetch(url, { method: "GET", credentials: "include" });
@@ -14,11 +14,17 @@ function App() {
   const { data: auth } = useSWR(authUrl, fetcher);
   const { data: oppfolging } = useSWR(auth && auth.securityLevel === "4" ? oppfolgingUrl : null, fetcher);
   const { data: besvarelse } = useSWR(auth && auth.securityLevel === "4" ? besvarelseUrl : null, fetcher);
+  const { data: registrering } = useSWR(auth && auth.securityLevel === "4" ? registreringUrl : null, fetcher);
   const { data: underOppfolging } = useSWR(auth && auth.securityLevel === "3" ? underOppfolgingUrl : null, fetcher);
 
   return (
     <div className="podlet-vta-jobb">
-      <Jobb underOppfolging={underOppfolging} oppfolging={oppfolging} besvarelse={besvarelse} />
+      <Jobb
+        underOppfolging={underOppfolging}
+        oppfolging={oppfolging}
+        besvarelse={besvarelse}
+        registrering={registrering}
+      />
     </div>
   );
 }
