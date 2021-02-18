cb88dde3b8bb17436ab7c8bbae6e71590cb4ebbe
Julian Ravn Thrap-Meyer <Julian.Ravn.Thrap-Meyer@nav.no>
2021-02-18 11:08:22 +0100

    Fjerner utkommentert web-vitals

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { SWRConfig } from "swr";
-//import reportWebVitals from "./reportWebVitals";
 
 if (process.env.NODE_ENV === "development") {
   const { worker } = require("./mocks/browser");
@@ -18,8 +17,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("podlet-vta-jobb")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-//reportWebVitals(console.log);
